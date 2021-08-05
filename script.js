a679f989e2c9fb6bfcb8474e5bf64d204fa338b9
conjurer-23 <86053671+conjurer-23@users.noreply.github.com>
2021-08-05 14:35:44 +0530

    done

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 const btnCheck = document.querySelector(".check");
 const btnAgain = document.querySelector(".again");
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 btnCheck.addEventListener("click", function () {
@@ -17,6 +16,7 @@ btnCheck.addEventListener("click", function () {
     // When the player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct number!";
+    document.querySelector(".number").textContent = secretNumber;
 
     document.querySelector("body").style.backgroundColor = "#60b347";
 
@@ -44,4 +44,17 @@ btnCheck.addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+// Pressing the again button
+btnAgain.addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector(".message").textContent = "Starts guessing";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
