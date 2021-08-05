442e9456116d177ca0f3d5574db1583e288f1727
conjurer-23 <86053671+conjurer-23@users.noreply.github.com>
2021-08-05 13:47:16 +0530

    Added Click Event
    Implemented Game Logic

--- script.js
+++ script.js
@@ -3,14 +3,34 @@
 const btnCheck = document.querySelector(".check");
 const btnAgain = document.querySelector(".again");
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+let score = 20;
 
 btnCheck.addEventListener("click", function () {
-  console.log(document.querySelector(".guess").value);
+  const guess = Number(document.querySelector(".guess").value);
 
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No Number!";
-  } else {
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct number!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "👆 Guess is high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "😢 You lost game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "👇 Guess is low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "😢 You lost game!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
