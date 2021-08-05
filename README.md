ba7493d89896102c1ad40b28144f0ec25ba1ce11
conjurer-23 <86053671+conjurer-23@users.noreply.github.com>
2021-08-05 16:24:45 +0530

    done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GuessTheNumber
+
 It's a very simple and very easy game in this game you've to just guess the correct number.
--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const btnAgain = document.querySelector(".again");
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 btnCheck.addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -21,7 +22,10 @@ btnCheck.addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
 
     document.querySelector(".number").style.width = "30rem";
-
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
     // When the guess is high
   } else if (guess > secretNumber) {
     if (score > 1) {
