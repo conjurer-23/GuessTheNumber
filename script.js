2186e4d0dd7de801f66e3392b297dad568db3e42
conjurer-23 <86053671+conjurer-23@users.noreply.github.com>
2021-08-05 14:14:54 +0530

    Added functionality of manipulating css style

--- script.js
+++ script.js
@@ -10,10 +10,19 @@ let score = 20;
 btnCheck.addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
+  // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No Number!";
+
+    // When the player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct number!";
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+
+    // When the guess is high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "👆 Guess is high!";
@@ -23,6 +32,8 @@ btnCheck.addEventListener("click", function () {
       document.querySelector(".message").textContent = "😢 You lost game!";
       document.querySelector(".score").textContent = 0;
     }
+
+    // When the guess is low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "👇 Guess is low!";
