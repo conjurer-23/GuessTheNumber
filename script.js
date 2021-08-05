8722b71e92d814b5a4bc09e5f6bbd4135df6fe0a
conjurer-23 <86053671+conjurer-23@users.noreply.github.com>
2021-08-05 13:07:29 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-'use strict';
+"use strict";
+
+const btnCheck = document.querySelector(".check");
+const btnAgain = document.querySelector(".again");
+
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = number;
+
+btnCheck.addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No Number!";
+  } else {
+  }
+});
